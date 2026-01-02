ad9e8b9b331425889f204bbc719efa36e5837ad2
arash <arashkarimi1999@gmail.com>
2026-01-02 23:06:57 +0100

    we're so done with koyeb

--- main.py
+++ main.py
@@ -84,9 +84,5 @@ async def main():
 
 
 if __name__ == "__main__":
-    # +---just to deploy on koyeb---+
-    import subprocess
-    process = subprocess.Popen(['python3', '-m', 'http.server', '8000'])
-    # +-----------------------------+
     asyncio.run(main())
 
