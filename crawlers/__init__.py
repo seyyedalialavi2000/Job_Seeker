6eb0c793c888f3a5f38333990f91822ca04bed54
arash <arashkarimi1999@gmail.com>
2026-06-01 02:35:19 +0200

    feat: add Deloitte to the companies import list

--- crawlers/__init__.py
+++ crawlers/__init__.py
@@ -1,4 +1,6 @@
 from crawlers.base import BaseCrawler
-from crawlers.companies import Siemens, SiemensEnergy, Fraunhofer
+from crawlers.companies import (
+    Siemens, SiemensEnergy, Fraunhofer, Accenture, Amazon, Deloitte
+)
 from crawlers.registry import CrawlerMetadata, CRAWLER_REGISTRY
 from crawlers.runner import run_crawler, register_crawler_tasks
